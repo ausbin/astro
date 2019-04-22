14daace19af3a309ad598a367f198b05e5890a1b
Austin Adams <git@austinjadams.com>
2019-04-21 22:39:10 -0500

    Implement more functions in toy list class

--- example/list.c
+++ example/list.c
@@ -1,15 +1,95 @@
 #include <stdlib.h>
 #include "list.h"
 
-list_t *list_new_good(void) {
+list_t *list_new(void) {
     return calloc(1, sizeof (list_t));
 }
 
-void list_free(list_t *list) {
-    (void)list;
+void list_free(list_t *list, list_free_func_t free_func) {
+    if (!list || !free_func)
+        return;
+
+    list_node_t *n = list->head;
+    while (n) {
+        list_node_t *next = n->next;
+        free_func(n->data);
+        free(n);
+        n = next;
+    }
+
+    free(list);
+    return;
 }
 
-list_t *list_deep_copy(list_t *list) {
-    (void)list;
+list_t *list_deep_copy(list_t *list, list_copy_func_t copy_func,
+                       list_free_func_t free_func) {
+    if (!list || !copy_func || !free_func)
+        return NULL;
+
+    list_t *new_list = list_new();
+    if (!new_list)
+        return NULL;
+
+    new_list->size = list->size;
+
+    list_node_t *new_prev = NULL;
+
+    for (list_node_t *n = list->head; n; n = n->next) {
+        list_node_t *new_node = malloc(sizeof (list_node_t));
+        if (!new_node)
+            goto failure;
+
+        void *new_data = copy_func(new_node->data);
+        if (new_node->data && !new_data) {
+            free(new_node);
+            goto failure;
+        }
+
+        new_node->data = new_data;
+        new_node->next = NULL;
+
+        if (!new_prev) {
+            new_list->head = new_node;
+        } else {
+            new_prev->next = new_node;
+        }
+
+        new_prev = new_node;
+    }
+
     return NULL;
+
+    failure:
+    list_free(new_list, free_func);
+    return NULL;
+}
+
+int list_push(list_t *list, void *data) {
+    if (!list)
+        return 0;
+
+    list_node_t *node = malloc(sizeof (list_node_t));
+    if (!node)
+        return 0;
+
+    node->data = data;
+    node->next = list->head;
+    list->head = node;
+    return 1;
+}
+
+int list_pop(list_t *list, void **data_out) {
+    if (!list || !data_out || !list->size) {
+        if (data_out)
+            *data_out = NULL;
+        return 0;
+    }
+
+    list_node_t *node = list->head;
+    list->head = node->next;
+
+    *data_out = node->data;
+    free(node);
+
+    return 1;
 }
--- example/list.h
+++ example/list.h
@@ -11,8 +11,14 @@ typedef struct {
     list_node_t *head;
 } list_t;
 
+typedef void (*list_free_func_t)(void *);
+typedef void *(*list_copy_func_t)(void *);
+
 extern list_t *list_new(void);
-extern void list_free(list_t *list);
-extern list_t *list_deep_copy(list_t *list);
+extern void list_free(list_t *list, list_free_func_t free_func);
+extern list_t *list_deep_copy(list_t *list, list_copy_func_t copy_func,
+                              list_free_func_t free_func);
+extern int list_push(list_t *list, void *data);
+extern int list_pop(list_t *list, void **data_out);
 
 #endif
