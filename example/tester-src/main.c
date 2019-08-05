57a3bf27ce08444b2f4eed695856eaa8178d18cd
Austin Adams <git@austinjadams.com>
2019-08-04 19:05:46 -0700

    Add --gdb option which is a no-op

--- example/tester-src/main.c
+++ example/tester-src/main.c
@@ -5,7 +5,9 @@
 #include "suites.h"
 
 static void print_usage(const char *prog) {
-    fprintf(stderr, "usage: %s <test_name>\n", prog);
+    fprintf(stderr, "usage: %s                    (run all tests)\n", prog);
+    fprintf(stderr, "       %s <test name>        (run single test)\n", prog);
+    fprintf(stderr, "       %s --gdb <test name>  (start gdb server for test)\n", prog);
     // TODO: print available tests
 }
 
@@ -22,12 +24,24 @@ static int run_test(tester_t *tester, test_t *test, int blank_line) {
     return !!astro_err;
 }
 
+static int run_test_gdb(tester_t *tester, test_t *test) {
+    (void)tester;
+    (void)test;
+
+    return 0;
+}
+
 int main(int argc, char **argv) {
     char *test_name = NULL;
+    bool gdb = false;
 
-    if (argc-1 == 1) {
+    int nargs = argc-1;
+    if (nargs == 1) {
         test_name = argv[1];
-    } else if (argc-1 > 1) {
+    } else if (nargs == 2 && !strcmp(argv[1], "--gdb")) {
+        test_name = argv[2];
+        gdb = true;
+    } else if (nargs > 2) {
         print_usage(argv[0]);
         return 1;
     }
@@ -40,9 +54,11 @@ int main(int argc, char **argv) {
 
     if (test_name) {
         test_t *test;
-        if (!(test = tester_get_test(tester, argv[1]))) {
-            fprintf(stderr, "error: unknown test `%s'\n", argv[1]);
+        if (!(test = tester_get_test(tester, test_name))) {
+            fprintf(stderr, "error: unknown test `%s'\n", test_name);
             exit_code = 1;
+        } else if (gdb) {
+            exit_code = run_test_gdb(tester, test);
         } else {
             exit_code = run_test(tester, test, 0);
         }
